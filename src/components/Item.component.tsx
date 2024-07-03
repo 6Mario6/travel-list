1354d94adcb2ff40ca29afb45579d640fee87245
6Mario6 <mario66fernandez@gmail.com>
2024-07-02 22:36:15 -0500

    Refactor Item component onClick event handler

--- src/components/Item.component.tsx
+++ src/components/Item.component.tsx
@@ -12,7 +12,7 @@ export const Item = ({
         {item.quantity} {item.description}
       </span>
       <button
-        onChange={() => {
+        onClick={() => {
           onDeleteItem(item.id);
         }}
       >

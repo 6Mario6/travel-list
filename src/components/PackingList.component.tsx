d13a55e181b92474376bf6e620e4878af3d53011
6Mario6 <mario66fernandez@gmail.com>
2024-07-08 14:28:36 -0500

    Add sorting functionality to PackingList component

--- src/components/PackingList.component.tsx
+++ src/components/PackingList.component.tsx
@@ -1,5 +1,6 @@
 import { Item } from "./Item.component";
 import { Item as ItemType } from "../types/item.type";
+import { useState } from "react";
 export const PackingList = ({
   items,
   onDeleteItem,
@@ -9,10 +10,25 @@ export const PackingList = ({
   onDeleteItem: (id: number) => void;
   onToggleItem: (id: number) => void;
 }) => {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems: ItemType[] = [];
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -21,6 +37,13 @@ export const PackingList = ({
           ></Item>
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 };
